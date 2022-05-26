7e2f193058bcc1feea89c16e424a2eeb521b5327
5hapeshifter <suleimandiverio@gmail.com>
2022-05-26 17:06:41 -0300

    bds03 concluded

---
+++ bds03/src/main/java/com/devsuperior/bds03/config/AppConfig.java
@@ -0,0 +1,45 @@
+package com.devsuperior.bds03.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+/**
+ * Classe de configuração com @Configuration
+ */
+@Configuration
+public class AppConfig {
+
+    @Value(value = "${jwt.secret}")
+    private String jwtSecret;
+
+    /**
+     * Bean é um componente do Spring, com o @Bean o Spring Boot passa a gerenciar a execução do método.
+     * Nesse caso esse objeto será utilizado para encriptar as senhas.
+     * O simples fato de ter incluído a dependência do Spring Security, a aplicação fica protegida e
+     * só podemos acessá-la com a senha que o Spring disponibiliza
+     */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    // Idealmente isso seria colocado em uma classe e pasta especifica de segurança, são objetos capazes de ler, decodificar e criar um token
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey(jwtSecret); // chave do token registrada
+        return tokenConverter;
+    }
+
+    @Bean
+    public JwtTokenStore tokenStore() {
+        // responsavel por acessar o token
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+}
---
+++ bds03/src/main/java/com/devsuperior/bds03/entities/User.java
@@ -0,0 +1,123 @@
+package com.devsuperior.bds03.entities;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements UserDetails, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER) // Anotação para forçar que sempre que um usuário for buscado no banco, vira os atributos de todas entidades associadas a ele
+    @JoinTable( // Anotação que cria uma tabel e faz a associação entre duas entidades
+            name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),  // joinColumns Estabelece a chave estrangeira relacionada a classe presente
+            inverseJoinColumns = @JoinColumn(name = "role_id")) // inverseJoinColumns Estabelece a outra chave estrangeira com base no objeto da coleção, nesse caso é Category
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    // Métodos implementados a partir da interface UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //Estamos convertendo todas as Roles para SimpleGrantedAuthority
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+                .collect(Collectors.toList());
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // como não utilizaremos esse método, vamos retornar true
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // como não utilizaremos esse método, vamos retornar true
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // como não utilizaremos esse método, vamos retornar true
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // como não utilizaremos esse método, vamos retornar true
+        return true;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+
+}
