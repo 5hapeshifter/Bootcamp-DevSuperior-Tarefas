38f3c8fb42ee1ce313dc86bd4b979cfac1031fab
5hapeshifter <suleimandiverio@gmail.com>
2022-05-27 15:11:59 -0300

    bds04 basic class implemented

---
+++ bds04/src/main/java/com/devsuperior/bds04/config/ResourceServerConfig.java
@@ -0,0 +1,56 @@
+package com.devsuperior.bds04.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableResourceServer // Classe responsavel por receber o token e verificar se o acesso da requisicao esta ok
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    // a partir desse objeto conseguimos acessar diversas variaveis da aplicacao
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    // rota que sera publica para acessar
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**","/cities/**", "/events/**"};
+
+    // rota com nivel de acesso
+    private static final String[] CLIENT_POST = {"/events/**"};
+
+    private static final String[] ADMIN_POST = {"/events/**", "/cities/**"};
+
+
+    // configuracao do token store, passamos o nosso bean para ele ser decodificado e analisado, verificar a validade
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    // configuracao das rotas
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        // configuracao para liberar o console do h2
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.POST, CLIENT_POST).hasAnyRole("CLIENT","ADMIN")
+                .anyRequest().hasAnyRole("ADMIN"); // aqui estamos exigindo que o usuario esteja logado para acessar qualquer outra rota
+
+    }
+}
---
+++ bds04/src/main/java/com/devsuperior/bds04/controllers/CityController.java
@@ -0,0 +1,13 @@
+package com.devsuperior.bds04.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cities")
+public class CityController {
+
+
+
+
+}
